ee1a099e705b0607b800dc9a7cea95d7891f3e0d
Heri <heri@studiozenkai.com>
2017-11-21 15:24:07 -0500

    Parsed make, reservation price of the day, trips, location, total revenues, amenities

--- explore.py
+++ explore.py
@@ -1,26 +1,45 @@
 import dryscrape
 import sys
+import bs4 as bs
+import re
 
 if 'linux' in sys.platform:
     # start xvfb in case no X is running. Make sure xvfb 
     # is installed, otherwise this won't work!
     dryscrape.start_xvfb()
 
-site = 'https://turo.com'
-sess = dryscrape.Session(base_url = site)
+sess = dryscrape.Session()
 sess.set_attribute('auto_load_images', False)
 
-sess.visit('/rentals/suvs/nj/jersey-city/land-rover-range-rover-sport/84266')
-name = sess.xpath("//p[@class='vehicleLabel-makeModel']//text()")
-if len(name) > 0:
-    print name[0]
-price = sess.xpath("//span[@class='reservationBoxVehiclePrice-amount']//text()")
-if len(price) > 0:
-    print price[0]
+sess.visit('https://turo.com/rentals/suvs/nj/jersey-city/land-rover-range-rover-sport/84266')
 
-trips = sess.xpath("//div[@class='starRating-ratingLabel']//text()")
-if len(trips) > 0:
-    print trips[0]
+page = bs.BeautifulSoup(sess.body(), 'lxml')
 
-sess.render('land-rover.png')
-print('Screenshot written to land-rover')
+names = page.find('p', class_='vehicleLabel-makeModel')
+if len(names) > 0:
+    name = names.text
+    print('Make Model: {name}'.format(name = name))
+
+    reservation_price = page.find('span', class_='reservationBoxVehiclePrice-amount').text
+    print('Reservation Price: ${reservation_price}'.format(reservation_price = reservation_price))
+    
+    trips = page.find('div', class_='starRating-ratingLabel')
+    total_trips = int(re.search(r'\d+', trips.text).group())
+    print('Trips: {total_trips}'.format( total_trips = total_trips ) )
+    
+    location = page.find('div', class_='vehicleMapDetailsItem-description').text
+    print('Location: {location}'.format(location = location))
+
+    revenues = total_trips * int(reservation_price)
+    print('Revenues: {revenues}'.format(revenues = '${:,.2f}'.format(revenues)))
+
+    amenities = page.findAll('div', class_='labeledBadge-label')
+    if len(amenities) > 0:
+        print("Amenities: ")
+        for amenity in amenities:
+            print(amenity.text)
+
+    # TODO : save name, reservation_price, total_trips, revenues, amenities city to database
+
+    sess.render('{name}.png'.format(name=name))
+    print('Screenshot written to {name}'.format(name=name))
