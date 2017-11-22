e9221899eaa920b0d91fe5721bd87c050445be6b
Heri <heri@studiozenkai.com>
2017-11-22 15:48:36 -0500

    Added current date, amenities in list form, and DRY format

--- explore.py
+++ explore.py
@@ -2,11 +2,20 @@ import dryscrape
 import sys
 import bs4 as bs
 import re
+import datetime
 
 if 'linux' in sys.platform:
     # start xvfb in case no X is running. Make sure xvfb 
     # is installed, otherwise this won't work!
     dryscrape.start_xvfb()
+
+def convert_to_int(search):
+    return int(re.search(r'\d+', search).group())
+
+def get_from_page(page, name, element, elementClass):
+    result = page.find(element, class_=elementClass)
+    print("{name}: {result}".format(name=name, result=result.text))
+    return result.text
 
 sess = dryscrape.Session()
 sess.set_attribute('auto_load_images', False)
@@ -15,31 +24,31 @@ sess.visit('https://turo.com/rentals/suvs/nj/jersey-city/land-rover-range-rover-
 
 page = bs.BeautifulSoup(sess.body(), 'lxml')
 
-names = page.find('p', class_='vehicleLabel-makeModel')
-if len(names) > 0:
-    name = names.text
-    print('Make Model: {name}'.format(name = name))
+name = get_from_page(page, 'Make Model', 'p', 'vehicleLabel-makeModel')
 
-    reservation_price = page.find('span', class_='reservationBoxVehiclePrice-amount').text
-    print('Reservation Price: ${reservation_price}'.format(reservation_price = reservation_price))
-    
-    trips = page.find('div', class_='starRating-ratingLabel')
-    total_trips = int(re.search(r'\d+', trips.text).group())
-    print('Trips: {total_trips}'.format( total_trips = total_trips ) )
-    
-    location = page.find('div', class_='vehicleMapDetailsItem-description').text
-    print('Location: {location}'.format(location = location))
+amenities = [amenity.text for amenity in page.findAll('div', class_='labeledBadge-label')]
+if len(amenities) > 0:
+    print("Amenities: {amenities}".format(amenities = ", ".join(amenities)))
 
-    revenues = total_trips * int(reservation_price)
-    print('Revenues: {revenues}'.format(revenues = '${:,.2f}'.format(revenues)))
+# mileage = page.find('div', class_='reservationBoxMileage-distance')
+# included_mileage = convert_to_int(mileage.text)
+# print('Included mileage: {included_mileage}'.format(included_mileage = included_mileage))
 
-    amenities = page.findAll('div', class_='labeledBadge-label')
-    if len(amenities) > 0:
-        print("Amenities: ")
-        for amenity in amenities:
-            print(amenity.text)
+location = get_from_page(page, 'Location', 'div', 'vehicleMapDetailsItem-description')
 
-    # TODO : save name, reservation_price, total_trips, revenues, amenities city to database
+today = datetime.date.today()
+print('Date: {date}'.format(date = today.strftime('%d, %b %Y')))
 
-    sess.render('{name}.png'.format(name=name))
-    print('Screenshot written to {name}'.format(name=name))
+reservation_price = get_from_page(page, 'Reservation Price', 'span', 'reservationBoxVehiclePrice-amount')
+
+trips = page.find('div', class_='starRating-ratingLabel')
+total_trips = convert_to_int(trips.text)
+print('Trips: {total_trips}'.format( total_trips = total_trips ) )
+
+revenues = total_trips * int(reservation_price)
+print('Estimated Revenues: {revenues}'.format(revenues = '${:,.2f}'.format(revenues)))
+
+# TODO : save name, reservation_price, total_trips, revenues, amenities city to database
+
+sess.render('{name}.png'.format(name=name))
+print('Screenshot written to {name}'.format(name=name))
